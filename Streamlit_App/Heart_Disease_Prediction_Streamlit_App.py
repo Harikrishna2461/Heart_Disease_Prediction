79164bd01990d25ecc3091b6680739eea1fbf391
Hari Krishna Dhamodaran <67218513+Harikrishna2461@users.noreply.github.com>
2023-09-20 22:49:33 +0530

    Update Heart_Disease_Prediction_Streamlit_App.py

--- Streamlit_App/Heart_Disease_Prediction_Streamlit_App.py
+++ Streamlit_App/Heart_Disease_Prediction_Streamlit_App.py
@@ -8,32 +8,25 @@ import base64
 import os
 
 bgm_path = os.path.join(os.path.dirname(__file__), "bgm.png")
-
-@st.cache(suppress_st_warning=True)
+# Define set_bg_hack function without st.markdown
 def set_bg_hack(main_bg):
-    '''
-    A function to unpack an image from root folder and set as bg.
- 
-    Returns
-    -------
-    The background.
-    '''
-    # set bg name
     main_bg_ext = "png"
-        
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
+    return main_bg_ext
 
-set_bg_hack(bgm_path)
+# Call set_bg_hack and use st.markdown outside the function
+main_bg_ext = set_bg_hack(bgm_path)
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(bgm_path, "rb").read()).decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+#set_bg_hack(bgm_path)
 
 # Load the TensorFlow Lite model
 model_path = os.path.join(os.path.dirname(__file__), "model_int8_latest.tflite")
