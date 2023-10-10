73c54cff7538c1abf76e544d88398591320e520c
Hari Krishna Dhamodaran <67218513+Harikrishna2461@users.noreply.github.com>
2023-10-10 02:15:51 -0700

    Update Final_App.py

--- Final_Webapp_Implementation/Final_App.py
+++ Final_Webapp_Implementation/Final_App.py
@@ -9,12 +9,13 @@ import base64
 import os
 
 bgm_path = os.path.join(os.path.dirname(__file__), "White_BGM.png")
+
 # Define set_bg_hack function without st.markdown
 def set_bg_hack(main_bg):
     main_bg_ext = "png"
     return main_bg_ext
 
-#Call set_bg_hack and use st.markdown outside the function
+# Call set_bg_hack and use st.markdown outside the function
 main_bg_ext = set_bg_hack(bgm_path)
 st.markdown(
     f"""
@@ -27,36 +28,64 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-#set_bg_hack(bgm_path)
 
 # Load the TensorFlow Lite model
 model_path = os.path.join(os.path.dirname(__file__),"autokeras_synch_sft_model.tflite")
 interpreter = tf.lite.Interpreter(model_path)
 interpreter.allocate_tensors()
 
-# Function for preprocessing sound wave into an image
-def sound_data_to_image_loading_and_preprocessing_chirplet(file_path):
-    sample_s, sr = librosa.load(file_path, sr=32000, mono=True)
-    melspec = librosa.feature.melspectrogram(y=sample_s, sr=sr)
-    melspec = librosa.power_to_db(melspec).astype(np.float32)
-    w = chirp(melspec, f0=6, f1=1, t1=10, method='linear')
-    Height = w.shape[0]
-    Width = w.shape[1]
-    img1 = Image.fromarray(w, "I")
-    img1 = np.array(img1)
-    arr1 = img1.reshape((Height, Width, 1))
-    arr2 = np.array(arr1, dtype='uint8')
-    resized_img = cv2.resize(arr2, (128, 128))
-    resized_img = resized_img.reshape((128, 128, 1))
-    c_rgb_img = cv2.cvtColor(resized_img, cv2.COLOR_GRAY2RGB)
-    c_rgb_img = np.array(c_rgb_img)
-    return c_rgb_img
+# Functions for preprocessing sound wave into an image
+# Synchrosqueezing
+def synchrosqueeze(matrix, t):
+    inst_freq = np.abs(np.gradient(np.angle(matrix), axis=0))
+    synchrosqueezed = np.zeros_like(matrix, dtype=complex)
+    for i in range(matrix.shape[0]):
+        synchrosqueezed[i, :] = matrix[i, :] * np.exp(2j * np.pi * t * inst_freq[i, :])
+    return synchrosqueezed
+    
+def SFT(path, window_sizes):
+    sample_rate, audio_data = wavfile.read(path)
+
+    results = []
+    for fft_size in window_sizes:
+        overlap_fac = 0.5
+        hop_size = np.int32(np.floor(fft_size * (1 - overlap_fac)))
+        pad_end_size = fft_size
+        total_segments = np.int32(np.ceil(len(audio_data) / np.float32(hop_size)))
+        t_max = len(audio_data) / np.float32(sample_rate)
+
+        window = np.hanning(fft_size)
+        inner_pad = np.zeros(fft_size)
+
+        proc = np.concatenate((audio_data, np.zeros(pad_end_size)))
+        result = np.empty((total_segments, fft_size), dtype=np.float32)
+
+        for i in range(total_segments):
+            current_hop = hop_size * i
+            segment = proc[current_hop:current_hop + fft_size]
+            windowed = segment * window
+            padded = np.append(windowed, inner_pad)
+            spectrum = np.fft.fft(padded) / fft_size
+            autopower = np.abs(spectrum * np.conj(spectrum))
+            result[i, :] = autopower[:fft_size]
+
+        result = 20 * np.log10(result)
+        result = np.clip(result, -40, 200)
+
+        synchrosqueezed_result = synchrosqueeze(result.T, np.arange(result.shape[0]))
+
+        resized_spectrogram = cv2.resize(np.abs(synchrosqueezed_result), (128, 128), interpolation=cv2.INTER_LINEAR)
+        resized_spectrogram = (255 * (resized_spectrogram - resized_spectrogram.min()) / (resized_spectrogram.max() - resized_spectrogram.min())).astype(np.uint8)
+        colormap = plt.get_cmap('viridis')
+        spectrogram_rgb = colormap(resized_spectrogram)[:, :, :3]
+
+        results.append(spectrogram_rgb)
+
+    return results
+
 
 # Streamlit UI
-st.title(":red[Heart Disease Prediction from Heart Beat Sound Wave]")
-# Change the title color to red
-#st.markdown("<span style='color:red'>Custom Color Title</span>", unsafe_allow_html=True)
-
+st.title(":green[Heart Disease Prediction from Heart Beat Sound Wave]")
 
 # Upload sound wave file
 uploaded_file = st.file_uploader(":green[Upload a sound wave file (.wav)]", type=["wav"])
@@ -67,7 +96,8 @@ predict_button = st.button("Predict the Disease")
 if uploaded_file and predict_button:
     try:
         # Preprocess the sound wave into an image
-        input_image = sound_data_to_image_loading_and_preprocessing_chirplet(uploaded_file)
+        window_sizes = [512]
+        input_image = SFT(uploaded_file, window_sizes)
 
         if predict_button:  # Check if the button is pressed
             # Make predictions using the TensorFlow Lite model
